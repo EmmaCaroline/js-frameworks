b60b515d33440e41cbd210f0bbe6cd65cb30e57f
EmmaCaroline <emma-caroline@hotmail.com>
2025-03-08 19:18:48 +0100

    Comment clean up

--- src/components/ui/ProductCard.jsx
+++ src/components/ui/ProductCard.jsx
@@ -2,12 +2,10 @@ import React from "react";
 import { FaStar, FaRegStar, FaStarHalfAlt } from "react-icons/fa";
 
 const ProductCard = ({ product }) => {
-  // Calculate the discount if there is a difference between price and discountedPrice
   const discount = product.price - product.discountedPrice;
   const discountPercentage =
     discount > 0 ? Math.round((discount / product.price) * 100) : 0;
 
-  // Calculate the number of full stars, half stars, and empty stars
   const fullStars = Math.floor(product.rating);
   const halfStar = product.rating % 1 >= 0.5 ? 1 : 0;
   const emptyStars = 5 - fullStars - halfStar;
@@ -20,7 +18,6 @@ const ProductCard = ({ product }) => {
           alt={product.image.alt}
           className="w-56 h-56 sm:w-64 sm:h-64 xl:w-80 xl:h-80 aspect-square object-cover rounded-lg transition duration-300 ease-linear group-hover:scale-105 border border-gray-300"
         />
-        {/* Display the discount percentage in the top corner */}
         {discount > 0 && (
           <div className="absolute top-2 left-2 bg-red-500 text-white px-2 py-1 text-xs rounded-full">
             -{discountPercentage}%
@@ -31,13 +28,11 @@ const ProductCard = ({ product }) => {
       <h2 className="text-lg font-semibold mt-2">{product.title}</h2>
       <p className="text-gray-700 w-56 xl:w-72">{product.description}</p>
 
-      {/* Show price and discount if applicable */}
       <div className="mt-4 flex items-center">
         <span className="text-lg font-semibold text-gray-900">
           ${product.discountedPrice.toFixed(2)}
         </span>
 
-        {/* Display original price if there's a discount */}
         {discount > 0 && (
           <>
             <span className="line-through text-gray-500 ml-2">
@@ -47,12 +42,11 @@ const ProductCard = ({ product }) => {
         )}
       </div>
 
-      {/* Display rating stars */}
       <div className="flex mt-2">
         {Array(fullStars)
           .fill(<FaStar className="text-yellow-400" />)
           .map((star, index) => (
-            <span key={`full-${index}`}>{star}</span> // Add a unique key for each full star
+            <span key={`full-${index}`}>{star}</span>
           ))}
         {halfStar === 1 && (
           <span key="half">
@@ -62,7 +56,7 @@ const ProductCard = ({ product }) => {
         {Array(emptyStars)
           .fill(<FaRegStar className="text-yellow-400" />)
           .map((star, index) => (
-            <span key={`empty-${index}`}>{star}</span> // Add a unique key for each empty star
+            <span key={`empty-${index}`}>{star}</span>
           ))}
       </div>
     </div>
